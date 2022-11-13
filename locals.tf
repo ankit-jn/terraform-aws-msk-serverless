a9de64860933ac80e9aef9727d1a9a25f82ecb99
Ankit Jain <ankii.jain@gmail.com>
2022-11-13 21:48:50 +0530

    fis issue

--- locals.tf
+++ locals.tf
@@ -1,3 +1,4 @@
+locals{
     sg_name = coalesce(var.sg_name, format("%s-sg", var.cluster_name))
     
     sg_ingress_rules = flatten([ for rule_key, rule in var.sg_rules :  rule if rule_key == "ingress" ])
@@ -6,3 +7,4 @@
     additional_sg = coalesce(var.additional_sg, [])
     security_groups = var.create_sg ? concat([module.msk_security_group[0].security_group_id], 
                                                                 local.additional_sg) : local.additional_sg
+}
